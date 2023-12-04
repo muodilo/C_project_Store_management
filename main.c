da45575fd481df3137ba78338641fbbb1ee681aa
odilo <odilomurindahabi@gmail.com>
2023-12-04 15:52:46 +0200

    "Implement Display function to show available products from 'ItemsCollections.txt'. The function reads and formats data from the file, displaying ID, name, and price."

--- main.c
+++ main.c
@@ -9,6 +9,27 @@ struct Items{
   int price;
 } i;
 
+void Display(void){
+  FILE *p;
+  p= fopen("ItemsCollections.txt", "r");
+  printf("Display available Products\n------------------------\n\n");
+  printf("ID\tNAME\tPRICE");
+  printf("\n--\t----\t\t-----\n");
+  while(fscanf(p,"%d\t%[^\t]%*c\t%d\n",&i.id,i.name,&i.price)!=EOF){
+      if(strlen(i.name)<=8)
+    {
+      printf("\n%d\t%s    \t%d", i.id,i.name,i.price);
+    }
+    else
+    {
+      printf("\n%d\t%s\t%d", i.id,i.name,i.price);
+    }
+  }
+    fclose(p);
+    printf("\nPress any key to continue.\n");
+    getch();
+  
+}
 main(){
 
 }
